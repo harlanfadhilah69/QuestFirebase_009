dbbb1d720df38d5717517cc221f705b87a7c12c6
harlanfadhilah69 <harlancoegg@gmail.com>
2026-01-02 20:46:38 +0700

    implementasi repositori untuk integrasi Firebase Firestore

---
+++ app/src/main/java/com/example/questfirebasee_009/repositori/RepositorySiswa.kt
@@ -0,0 +1,15 @@
+package com.example.questfirebasee_009.repositori
+
+import com.example.questfirebasee_009.modeldata.Siswa
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+interface RepositorySiswa {
+    suspend fun getDataSiswa(): List<Siswa>
+    suspend fun postDataSiswa(siswa: Siswa)
+}
+
+class FirebaseRepositorySiswa : RepositorySiswa {
+    private val db = FirebaseFirestore.getInstance()
+    private val collection = db.collection("siswa")
+
