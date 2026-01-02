0a550c442c79886d4536b097c3cff3f5c751c6c4
harlanfadhilah69 <harlancoegg@gmail.com>
2026-01-02 20:48:55 +0700

    implementasi logika validasi input dan simpan data siswa

--- app/src/main/java/com/example/questfirebasee_009/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/questfirebasee_009/viewmodel/EntryViewModel.kt
@@ -13,3 +13,29 @@ class EntryViewModel(private val repositorySiswa: RepositorySiswa) : ViewModel()
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa.detailSiswa
+    ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(
+                detailSiswa = detailSiswa,
+                isEntryValid = validasiInput(detailSiswa)
+            )
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            repositorySiswa.postDataSiswa(
+                uiStateSiswa.detailSiswa.toDataSiswa()
+            )
+        }
+    }
+}
