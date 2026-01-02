a23a9a8468af3d2ceb8c6281f2603267f26f86e9
harlanfadhilah69 <harlancoegg@gmail.com>
2026-01-02 20:32:52 +0700

    membuat rute navigasi untuk halaman utama

---
+++ app/src/main/java/com/example/questfirebasee_009/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questfirebasee_009.view.route
+
+import com.example.questfirebasee_009.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
