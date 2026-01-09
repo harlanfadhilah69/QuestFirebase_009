e1da83592128be99d40f068fd628b5648d6ed2eb
harlanfadhilah69 <harlancoegg@gmail.com>
2026-01-09 18:25:57 +0700

    buat sealed interface StatusUIDetail untuk manajemen state layar detail

---
+++ app/src/main/java/com/example/questfirebasee_009/viewmodel/DetailViewModel.kt
@@ -0,0 +1,23 @@
+@file:OptIn(InternalSerializationApi::class)
+
+package com.example.questfirebasee_009.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase_009.view.route.DestinasiDetail
+import com.example.questfirebasee_009.modeldata.Siswa
+import com.example.questfirebasee_009.repositori.RepositorySiswa
+import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
+import java.io.IOException
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
