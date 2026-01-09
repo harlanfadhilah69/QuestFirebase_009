5096cb021a9c855cf53aa27c2fe987313ce7ccd9
harlanfadhilah69 <harlancoegg@gmail.com>
2026-01-09 18:30:59 +0700

    inisialisasi EditViewModel untuk fitur pembaruan data

---
+++ app/src/main/java/com/example/questfirebasee_009/viewmodel/EditViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.questfirebasee_009.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase_009.view.route.DestinasiDetail
+import com.example.questfirebasee_009.modeldata.DetailSiswa
+import com.example.questfirebasee_009.modeldata.UIStateSiswa
+import com.example.questfirebasee_009.modeldata.toDataSiswa
+import com.example.questfirebasee_009.modeldata.toUiStateSiswa
+import com.example.questfirebasee_009.repositori.RepositorySiswa
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+
