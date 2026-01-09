06e6c3afeeb64eaf2eefc657cc8453da2b5221e9
harlanfadhilah69 <harlancoegg@gmail.com>
2026-01-09 18:26:50 +0700

    inisialisasi DetailViewModel dengan SavedStateHandle dan Repository

--- app/src/main/java/com/example/questfirebasee_009/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebasee_009/viewmodel/DetailViewModel.kt
@@ -21,3 +21,8 @@ sealed interface StatusUIDetail {
     object Loading : StatusUIDetail
 }
 
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
