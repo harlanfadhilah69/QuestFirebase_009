088b366ae8e67e13545b6ba7047f529af112b0db
harlanfadhilah69 <harlancoegg@gmail.com>
2026-01-09 18:10:50 +0700

    atur tata letak konten di BodyDetailDataSiswa menggunakan Column

--- app/src/main/java/com/example/questfirebasee_009/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questfirebasee_009/view/HalamanDetail.kt
@@ -103,3 +103,8 @@ private fun BodyDetailDataSiswa(
     onDelete: () -> Unit,
     modifier: Modifier = Modifier
 ){
+    Column (
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen
+            .padding_medium))
+    ){
