0b905e6df602e791394113b64e046ac460c5d354
harlanfadhilah69 <harlancoegg@gmail.com>
2026-01-09 18:32:06 +0700

    implementasi pemuatan data awal dan inisialisasi state di EditViewModel

--- app/src/main/java/com/example/questfirebasee_009/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questfirebasee_009/viewmodel/EditViewModel.kt
@@ -19,4 +19,17 @@ class EditViewModel(
     private val repositorySiswa: RepositorySiswa
 ) : ViewModel() {
 
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
 
