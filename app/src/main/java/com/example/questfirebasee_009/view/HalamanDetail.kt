8acbaefda37b2eefb467024243aae2746d5a3dbe
harlanfadhilah69 <harlancoegg@gmail.com>
2026-01-09 18:13:57 +0700

    atur tata letak konten di dalam Card DetailDataSiswa

--- app/src/main/java/com/example/questfirebasee_009/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questfirebasee_009/view/HalamanDetail.kt
@@ -144,3 +144,9 @@ fun DetailDataSiswa(
             contentColor = MaterialTheme.colorScheme.onPrimaryContainer
         )
     ){
+        Column (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+        ){
