eff270f879d52685cbbe22517f11552ce05a0daa
harlanfadhilah69 <harlancoegg@gmail.com>
2026-01-09 18:18:18 +0700

    implementasi struktur dasar EditSiswaScreen

---
+++ app/src/main/java/com/example/questfirebasee_009/view/HalamanEdit.kt
@@ -0,0 +1,23 @@
+package com.example.questfirebasee_009.view
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.firebase_009.viewmodel.PenyediaViewModel
+import com.example.questfirebasee_009.view.route.DestinasiEdit
+import com.example.questfirebasee_009.viewmodel.EditViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+)
